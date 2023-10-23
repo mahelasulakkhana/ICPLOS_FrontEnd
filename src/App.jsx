53e8408c18304e714f700a72ee23fb830406b899
mahelasulakkhana <mahelasulakkahan1998.gmail.com>
2023-10-23 12:06:00 +0530

    route home page and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   
   return (
-    <>
-      <div className="w-screen h-screen bg-red-300">
-        
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Navbar' element={<Navbar/>}/>
+      </Routes>
+    </div>
   )
 }
 
